ef39b47c98d8aeefa195c93e27504274f2ec6f82
shah1012 <69828183+shah1012@users.noreply.github.com>
2020-09-23 21:26:21 -0500

    Your awesome message

--- src/Recipe.js
+++ src/Recipe.js
@@ -11,7 +11,7 @@ const Recipe = ({title, calories, image, ingredients, url }) => {
                 ))}
             </ol>
             <p>{calories}</p>
-            <a href={url}>Link to instructions it works hi</a>
+            <a href={url}>Link to  it works hi</a>
             <img className={Style.image} src={image} alt=""/>
 
         </div>
